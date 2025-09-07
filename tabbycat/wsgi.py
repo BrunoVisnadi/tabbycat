5c8f80e77ed845f2661473842a6a88250b0e3b34
bruno <brunovisnadi1@gmail.com>
2025-09-07 16:01:18 -0300

    sem worker

--- tabbycat/wsgi.py
+++ tabbycat/wsgi.py
@@ -2,8 +2,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# Needed for WSGI Callable
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-# Needed for waitress/gunicorn serving
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tabbycat.settings")
 application = get_wsgi_application()
